9a691ae3cb61d1d4afd8125c6941cb9135504a4e
Yu <59269129+yu-ko-ba@users.noreply.github.com>
2021-01-29 16:38:54 +0900

    -versionより後の引数が全てバージョンの値として読み込まれていたバグと、絶対パスで実行できないバグを修正した

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# These codes are licensed under CC0.
-# http://creativecommons.org/publicdomain/zero/1.0/deed.ja
-
-
 # 引数を解析する
 version="1.16.5"
 xms="-Xms2G"
@@ -12,37 +8,45 @@ nogui=""
 
 version_flag=false
 for i in $*; do
-	# バージョンが指定されていたら取得する
-	if ${version_flag}; then
-		version=${i}
-		continue
-	fi
-	if [ ${i} = "-version" ]; then
-		version_flag=true
-		continue
-	fi
+    # バージョンが指定されていたら取得する
+    if ${version_flag}; then
+        version_flag=false
 
-	# メモリサイズの最小値が指定されていたら取得する
-	if [ ${i:0:4} = "-Xms" ]; then
-		xms=${i}
-		continue
-	fi
+        version=${i}
+        continue
+    fi
+    if [ ${i} = "-version" ]; then
+        version_flag=true
+        continue
+    fi
 
-	# メモリサイズの最大値が指定されていたら取得する
-	if [ ${i:0:4} = "-Xmx" ]; then
-		xmx=${i}
-		continue
-	fi
+    # メモリサイズの最小値が指定されていたら取得する
+    if [ ${i:0:4} = "-Xms" ]; then
+        xms=${i}
+        continue
+    fi
 
-	# noguiが指定されていたら取得する
-	if [ ${i} = "nogui" ]; then
-		nogui="nogui"
-		continue
-	fi
+    # メモリサイズの最大値が指定されていたら取得する
+    if [ ${i:0:4} = "-Xmx" ]; then
+        xmx=${i}
+        continue
+    fi
+
+    # noguiが指定されていたら取得する
+    if [ ${i} = "nogui" ]; then
+        nogui="nogui"
+        continue
+    fi
 done
 
 # このシェルスクリプトが置いてあるディレクトリに移動する
-cd $(dirname "$(pwd)/${0}")
+if [ ${0:0:1} = "/" ]; then
+    # 絶対パスで実行されている場合
+    cd $(dirname "${0}")
+else
+    # 相対パスで実行されている場合
+    cd $(dirname "$(pwd)/${0}")
+fi
 
 # サーバーを起動する
 java -Xms2G -Xmx2G -jar "spigot-${version}.jar" ${nogui}
